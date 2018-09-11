3532daa88ee91f1b85ca4069a6acbc44e8da8285
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2018-09-11 17:51:44 -0300

    Create the five principal fragments and style drawer header

--- app/src/main/java/com/example/dglozano/escale/MainActivity.java
+++ app/src/main/java/com/example/dglozano/escale/MainActivity.java
@@ -1,54 +1,100 @@
 package com.example.dglozano.escale;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
+import com.example.dglozano.escale.fragments.DietFragment;
+import com.example.dglozano.escale.fragments.HomeFragment;
+import com.example.dglozano.escale.fragments.MessagesFragment;
+import com.example.dglozano.escale.fragments.SettingsFragment;
+import com.example.dglozano.escale.fragments.StatsFragment;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import q.rorbin.badgeview.Badge;
 import q.rorbin.badgeview.QBadgeView;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        MessagesFragment.OnFragmentInteractionListener{
 
-    private BottomNavigationViewEx bottomNavigationView;
+    private BottomNavigationViewEx mBnv;
+    private Badge mMessagesBadge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        bottomNavigationView = (BottomNavigationViewEx) findViewById(R.id.bnve);
-        bottomNavigationView.enableAnimation(true);
-        bottomNavigationView.enableShiftingMode(false);
-        bottomNavigationView.enableItemShiftingMode(false);
-        bottomNavigationView.setTextVisibility(false);
-        bottomNavigationView.setIconSizeAt(0, 28, 28);
-        addBadgeAt(3,10);
+        mBnv = findViewById(R.id.bnve);
+        mBnv.enableAnimation(true);
+        mBnv.enableShiftingMode(false);
+        mBnv.enableItemShiftingMode(false);
+        mBnv.setTextVisibility(false);
+        mBnv.setIconSizeAt(0, 28, 28);
+        //FIXME: como eliminar badge cuando veo ese fragment ?
+        mMessagesBadge = addBadgeAt(3,0);
+        mMessagesBadge.setBadgeNumber(10);
 
+        //Replace Default Fragment
+        FragmentManager manager = getSupportFragmentManager();
+        manager
+                .beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        mBnv.setOnNavigationItemSelectedListener((MenuItem item) -> {
+            switch (item.getItemId()) {
+                case R.id.action_home:
+                    manager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new HomeFragment())
+                            .commit();
+                    return true;
+                case R.id.action_stats:
+                    manager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new StatsFragment())
+                            .commit();
+                    return true;
+                case R.id.action_diet:
+                    manager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new DietFragment())
+                            .commit();
+                    return true;
+                case R.id.action_message:
+                    manager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new MessagesFragment())
+                            .commit();
+                    return true;
+                case R.id.action_settings:
+                    manager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new SettingsFragment())
+                            .commit();
+                    return true;
+            }
+            return false;
+        });
     }
 
     @Override
@@ -92,6 +138,11 @@ public class MainActivity extends AppCompatActivity
         return new QBadgeView(this)
                 .setBadgeNumber(number)
                 .setGravityOffset(12, 2, true)
-                .bindTarget(bottomNavigationView.getBottomNavigationItemView(position));
+                .bindTarget(mBnv.getBottomNavigationItemView(position));
+    }
+
+    @Override
+    public void onMessagesRead() {
+        mMessagesBadge.setBadgeNumber(0);
     }
 }
